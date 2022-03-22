ad5830b787192a58bf0b6e494a3cfff340aa7b05
tonomr <tonomac7@gmail.com>
2022-03-22 02:41:59 -0600

    finish using the template

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'antonio.magana7448@alumnos.udg.mx';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
